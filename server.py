d970bfb6440ce5104a466c9f2de94c0977254b1b
koseemre <temreo@hotmail.com>
2016-10-18 17:14:00 +0300

    Server Dosyasının Tekrar Düzenlenmesi

--- server.py
+++ server.py
@@ -1,15 +1,44 @@
 from flask import Flask
-from flask import Blueprint, render_template
+from flask import render_template
+from flask import redirect, url_for
+from flask import request
 
-from handlers import site
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('settings')
-    app.register_blueprint(site)
+@app.route('/')
+def home_page():
+    return render_template('home.html')
 
-    return app
+@app.route('/signup')
+def sign_up():
+    return render_template('giris.html')
 
+@app.route('/signin')
+def sign_in():
+    return render_template('girisyap.html')
+
+@app.route('/contactus')
+def contact():
+    return render_template('iletisim.html')
+
+@app.route('/signedin',methods=['GET', 'POST'])
+def signed_in():
+    if request.method == 'GET':
+        return render_template('profile/index.html')
+    else:
+        return redirect(url_for('app.signed_in'))
+
+@app.route('/profile')
+def profile():
+    return render_template('profile/profil.html')
+
+@app.route('/messages')
+def messages():
+    return render_template('profile/mesaj.html')
+
+@app.route('/friends')
+def friend_requests():
+        return render_template('profile/arkadas.html')
 
 if __name__ == '__main__':
     app = create_app()
