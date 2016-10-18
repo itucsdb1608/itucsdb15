e03c8989cabdffc16dc89fff4309433c2c119a8e
koseemre <temreo@hotmail.com>
2016-10-18 16:43:37 +0300

    Server Dosyasının Düzenlenmesi
    Server Dosyasının Bluemix'te Çalışmak Üzere Düzenlenmesi

--- server.py
+++ server.py
@@ -2,16 +2,49 @@ import datetime
 import os
 
 from flask import Flask
-from flask import render_template
-
+from flask import Blueprint, render_template
+from flask import redirect, url_for
+from flask import request
 
 app = Flask(__name__)
 
+site = Blueprint('site', __name__)
 
-@app.route('/')
+@site.route('/')
 def home_page():
-    now = datetime.datetime.now()
-    return render_template('home.html', current_time=now.ctime())
+    return render_template('home.html')
+
+@site.route('/signup')
+def sign_up():
+    return render_template('giris.html')
+
+@site.route('/signin')
+def sign_in():
+    return render_template('girisyap.html')
+
+@site.route('/contactus')
+def contact():
+    return render_template('iletisim.html')
+
+@site.route('/signedin',methods=['GET', 'POST'])
+def signed_in():
+    if request.method == 'GET':
+        return render_template('profile/index.html')
+    else:
+        return redirect(url_for('site.signed_in'))
+
+@site.route('/profile')
+def profile():
+    return render_template('profile/profil.html')
+
+@site.route('/messages')
+def messages():
+    return render_template('profile/mesaj.html')
+
+@site.route('/friends')
+def friend_requests():
+        return render_template('profile/arkadas.html')
+
 
 
 if __name__ == '__main__':
