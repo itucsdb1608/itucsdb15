a0db9e0ddbc7439479cade898e6dfda7f15dc00b
koseemre <temreo@hotmail.com>
2016-10-18 16:49:56 +0300

    Server Dosyasının Tekrar Düzenlenmesi
    Bluemix için server dosyasının düzenlenmesi

--- server.py
+++ server.py
@@ -1,53 +1,16 @@
-import datetime
-import os
-
 from flask import Flask
 from flask import Blueprint, render_template
-from flask import redirect, url_for
-from flask import request
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('settings')
+    app.register_blueprint(site)
 
-site = Blueprint('site', __name__)
-
-@site.route('/')
-def home_page():
-    return render_template('home.html')
-
-@site.route('/signup')
-def sign_up():
-    return render_template('giris.html')
-
-@site.route('/signin')
-def sign_in():
-    return render_template('girisyap.html')
-
-@site.route('/contactus')
-def contact():
-    return render_template('iletisim.html')
-
-@site.route('/signedin',methods=['GET', 'POST'])
-def signed_in():
-    if request.method == 'GET':
-        return render_template('profile/index.html')
-    else:
-        return redirect(url_for('site.signed_in'))
-
-@site.route('/profile')
-def profile():
-    return render_template('profile/profil.html')
-
-@site.route('/messages')
-def messages():
-    return render_template('profile/mesaj.html')
-
-@site.route('/friends')
-def friend_requests():
-        return render_template('profile/arkadas.html')
-
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
