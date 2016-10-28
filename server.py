06aec714b602a81d8c1268baa1bde8fff544e7c7
artuncF <fuartunc@gmail.com>
2016-10-29 01:03:07 +0300

    Server.py is beautified

--- server.py
+++ server.py
@@ -3,12 +3,16 @@ import os
 from page_handlers import site
 from flask import Flask
 from connect_db import connect_and_init
+from Onboard.OnboardGtk import app
 
-app = Flask(__name__)
-app.register_blueprint(site)
-app.connect_and_init = connect_and_init()
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(site)
+    app.connect_and_init = connect_and_init()
+    return app
 
 def main():
+    app=create_app()
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
