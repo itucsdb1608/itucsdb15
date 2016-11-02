7c1b6d07fd06411163777de27462f9625d1bf8e1
artuncF <fuartunc@gmail.com>
2016-11-03 02:32:35 +0300

    Page handlers updated for update and delete processes

--- page_handlers.py
+++ page_handlers.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 from flask import current_app
 from flask import request
 from flask import redirect, url_for
-from connect_db import add_message_to_table,get_messages_from_table,remove_message_from_table
+from connect_db import add_message_to_table,get_messages_from_table,remove_message_from_table,update_one_message
 from message import Message
 
 site = Blueprint('site', __name__)
@@ -22,6 +22,25 @@ def sign_in():
 def contact():
     return render_template('iletisim.html')
 
+@site.route('/message/update',methods=['GET','POST'])
+def update_message():
+    if request.method == 'GET':
+        return render_template('profile/update_message.html')
+    else:
+        username = request.form['username']
+        content = request.form['content']
+        subject = request.form['subject']
+        update_one_message(content,subject,username)
+        return redirect(url_for('site.signed_in'))
+
+@site.route('/message/delete',methods=['GET','POST'])
+def delete_message():
+    if request.method == 'GET':
+        return render_template('profile/delete_message.html')
+    else:
+        username = request.form['username']
+        remove_message_from_table(username)
+        return redirect(url_for('site.signed_in'))
 @site.route('/message/add',methods=['GET','POST'])
 def add_message():
     if request.method == 'GET':
